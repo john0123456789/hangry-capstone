d5aa0673ee38aa74335799a520f8705fa94acec2
John <john@DESKTOP-O1S0LR6.localdomain>
2022-08-03 13:57:04 -0700

    post crud for business functional

--- app/api/business_routes.py
+++ app/api/business_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_login import login_required
 from app.models import db, Business, Image
+from app.forms.business_form import BusinessForm
 
 business_routes = Blueprint("businesses", __name__)
 
@@ -10,3 +11,26 @@ business_routes = Blueprint("businesses", __name__)
 def get_business():
     businesses = Business.query.all()
     return {"businesses": [business.to_dict() for business in businesses]}
+
+# CREATE BUSINESS ROUTE
+@business_routes.route("/create", methods=['POST'])
+@login_required
+def create_business():
+    form = BusinessForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+    # if form.validate_on_submit():
+    business = Business(
+            user_id=form.data["user_id"],
+            name=form.data["name"],
+            address=form.data["address"],
+            zipcode=form.data["zipcode"],
+            city=form.data["city"],
+            state=form.data["state"],
+            country=form.data["country"],
+            phone_number=form.data["phone_number"],
+            website=form.data["website"]
+        )
+
+    db.session.add(business)
+    db.session.commit()
+    return business.to_dict()
