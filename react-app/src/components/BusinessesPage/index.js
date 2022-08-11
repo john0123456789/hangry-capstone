97f904f4509a65927a5611154cc84dd4cea1fc79
John <john@DESKTOP-O1S0LR6.localdomain>
2022-08-11 10:34:48 -0700

    added browse business to navbar, redirects to business list page

--- react-app/src/components/BusinessesPage/index.js
+++ react-app/src/components/BusinessesPage/index.js
@@ -1,70 +1,36 @@
-// import { deleteBusinessThunk, getAllBusinessesThunk } from "../../store/business";
-// import { clearReviews } from "../../store/review";
-// import { useDispatch, useSelector} from "react-redux";
-// import { useHistory } from "react-router-dom";
-// import { useEffect } from "react";
+import { getAllBusinessesThunk } from "../../store/business";
+import { useDispatch, useSelector} from "react-redux";
+import { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import './index.css'
 
-// function BusinessesPage() {
-//     const dispatch = useDispatch();
-//     const history = useHistory();
+function BusinessesPage() {
+    const dispatch = useDispatch();
 
-//     const businesses = useSelector((state) => Object.values(state.businesses));
+    const businesses = useSelector((state) => Object.values(state.businesses));
 
 
-// useEffect(() => {
-//     dispatch(getAllBusinessesThunk());
-// }, [dispatch]);
+useEffect(() => {
+    dispatch(getAllBusinessesThunk());
+}, [dispatch]);
 
-// const deleteBusinessClick = (e) => {
-//     e.preventDefault();
-//     const buttonData = Number(e.target.id);
-//     for (const business of businesses) {
-//         if (business.id === buttonData) {
-//             dispatch(deleteBusinessThunk(business, buttonData))
-//             history.push("/businesses")
-//         }
-//     }
-// }
+return (
+    <>
+    <div>
+        <h1>Hangry? Check These Businesses Out!</h1>
+        { businesses.map(business =>{
+            return (
+                <div key={business.id}>
+                <NavLink className="name" to={`/business/${business.id}`}><b>{business.name}</b></NavLink>
+                <h2>Address:{business.address}, {business.city}, {business.state} {business.zipcode}</h2>
+                <h2>{business.phone_number}</h2>
+                <h2>{business.website}</h2>
+                </div>
+            )
+        })}
+    </div>
+    </>
+  );
+}
 
-// const editBusinessClick = (e) => {
-//     e.preventDefault();
-//     const buttonData = Number(e.target.id);
-//     history.push(`/businesses/${buttonData}`)
-// }
-
-// return (
-//     <>
-//     <div>
-//         <h1>BUSINESSES PAGE</h1>
-//         { businesses.map(business =>{
-//             const getReviewClick = async (e) => {
-//                 e.preventDefault();
-//                 await dispatch(clearReviews());
-//                 const business_id = Number(e.target.id);
-//                 history.push(`/reviews/business/${business_id}`)
-//             }
-//             const addReviewClick = (e) => {
-//                 e.preventDefault();
-//                 const business_id = Number(e.target.id);
-//                 history.push(`/reviews/create/${business_id}`)
-//             }
-//             return (
-//                 <div key={business.id}>
-//                 <h2>{business.name}</h2>
-//                 <h2>Address:{business.address}, {business.city}, {business.state} {business.zipcode}</h2>
-//                 <h2>{business.phone_number}</h2>
-//                 <h2>{business.website}</h2>
-//                 {/* <img alt="business_images" src={business.business_images.url}></img> */}
-//                 <button type="button" id={business.id} onClick={getReviewClick}>Reviews</button>
-//                 <button type="button" id={business.id} onClick={addReviewClick}>Add Review</button>
-//                 <button type="button" id={business.id} onClick={editBusinessClick}>Edit</button>
-//                 <button type="button" id={business.id} onClick={deleteBusinessClick}>Delete</button>
-//                 </div>
-//             )
-//         })}
-//     </div>
-//     </>
-//   );
-// }
-
-// export default BusinessesPage;
+export default BusinessesPage;
--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -5,6 +5,7 @@ import LogoutButton from './auth/LogoutButton';
 import CreateBusinessModal from './CreateBusinessPage/CreateBusinessModal';
 import LoginFormModal from './auth/LoginFormModal';
 import SignUpFormModal from './auth/SignUpFormModal';
+import { TbListSearch } from 'react-icons/tb'
 import './NavBar.css'
 
 const NavBar = () => {
@@ -16,19 +17,9 @@ const NavBar = () => {
     history.push('/')
   }
 
-  const toLogin = (e) => {
+  const browseClick = (e) => {
     e.preventDefault();
-    history.push('/login')
-  }
-
-  const toSignup = (e) => {
-    e.preventDefault();
-    history.push('/sign-up')
-  }
-
-  const createBusiness = (e) => {
-    e.preventDefault();
-    history.push('/businesses/create')
+    history.push('/businesses')
   }
 
   if  (user) {
@@ -39,6 +30,7 @@ const NavBar = () => {
           <img src="https://i.imgur.com/dxJVLEP.png" alt="logo" className="hangry-logo" onClick={homeClick}/>
         </div>
         <div>
+          <button type="button" className="userbuttons" onClick={browseClick}><TbListSearch size="30px"/><span className="tooltiptext3">Browse Businesses</span></button>
           <button type="button" className="userbuttons"><CreateBusinessModal/><span className="tooltiptext">Add Business</span></button>
           <button type="button" className="userbuttons"><LogoutButton/><span className="tooltiptext2">Logout</span></button>
         </div>
