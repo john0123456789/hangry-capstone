750bc0b1026cd2eb2f6593471210c6054e994a7d
John <john@DESKTOP-O1S0LR6.localdomain>
2022-08-13 14:33:00 -0700

    added dynamic star and review rating to businesses page

--- react-app/src/components/BusinessesPage/index.js
+++ react-app/src/components/BusinessesPage/index.js
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { clearReviews } from "../../store/review";
 import './index.css'
-// import StarRating from "./StarRating";
+import StarRating from "./StarRating";
 
 function BusinessesPage() {
     const dispatch = useDispatch();
@@ -25,7 +25,7 @@ return (
             return (
                 <div key={business.id}>
                 <NavLink className="name" to={`/business/${business.id}`}><b>{business.name}</b></NavLink>
-                {/* <StarRating business={business}/> */}
+                <StarRating business={business}/>
                 <h2>{business.address}, {business.city}, {business.state} {business.zipcode}</h2>
                 <h2>{business.phone_number}</h2>
                 <h2>{business.website}</h2>
